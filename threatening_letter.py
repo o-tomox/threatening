f7e273e8085f667aca240c4a3cecd2966ff6ab41
Satoshi <satoshi.o.xxx@gmail.com>
2013-09-24 11:59:36 +0900

    脅迫状クラスなどの雛形の作成

---
+++ threatening_letter.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+脅迫状を作成するためのクラス
+"""
+
+class Letter(object):
+	""" 脅迫状のクラス """
+	def __init__(self):
+		self.letter = None
+
+
+class ClippingStyle(object):
+	""" 新聞記事の貼り付け位置などのクラス """
+	def __init__(self):
+		pass
