69a4eb3059163d7fd9b54921f755c8252a11026e
Satoshi <satoshi.o.xxx@gmail.com>
2013-10-04 15:57:18 +0900

    脅迫状インスタンスの生成のための初期処理．サイズの計算．

--- threatening.py
+++ threatening.py
@@ -4,15 +4,25 @@
 脅迫状を作成するプログラム
 """
 
+from threatening_letter import Letter
+
 
 # 脅迫状を作成する	
 def makeletter(text, background, filename):
-	pass
+	# 文章を行毎，一文字に分ける
+	characters = [[character for character in line.rstrip()] for line in text.split("\n")]
+	width_num = max([len(line) for line in characters])
+	height_num = len(characters)
+
+	# 脅迫状インスタンスを生成する
+	letter = Letter(width_num, height_num)
+
+	print letter
 
 
 
 if __name__ == '__main__':
-	back_white(255, 255, 255)
+	back_white = (255, 255, 255)
 	text = "これは脅迫状。\nここにいろいろな文章を入れる。\n改行もできる。"
 	filename = "letter.png"
 	makeletter(text, back_white, filename)
--- threatening_letter.py
+++ threatening_letter.py
@@ -5,9 +5,24 @@
 """
 
 class Letter(object):
+	# 一つ一つの文字のサイズと文字間のマージン
+	CHARA_WIDTH = 300
+	CHARA_HEIGHT = 400
+	HOR_MARGIN = 15
+	VER_MARGIN = 30
+
 	""" 脅迫状のクラス """
-	def __init__(self):
+	def __init__(self, width_num, height_num):
 		self.letter = None
+		self._calculate_lettersize(width_num, height_num);
+
+	""" 脅迫状のサイズ（仮）を計算する """
+	def _calculate_lettersize(self, width_num, height_num):
+		self.letterwidth = Letter.CHARA_WIDTH * width_num + Letter.HOR_MARGIN * (width_num + 1)
+		self.letterheight = Letter.CHARA_HEIGHT * height_num + Letter.VER_MARGIN * (height_num + 1)
+
+	def __str__(self):
+		return "<Letter w:{0} h:{1}>".format(self.letterwidth, self.letterheight)
 
 
 class ClippingStyle(object):
